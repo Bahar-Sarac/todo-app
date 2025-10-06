35d0fbe8789aa5ede11b139d8410ea1fa7533af8
Bahar Sarac <bbhrsrc@gmail.com>
2025-10-06 23:21:36 +0300

    CRUD işlemleri tamamlandı, Async-Storage yüklendi

--- app/index.tsx
+++ app/index.tsx
@@ -1,113 +1,271 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Checkbox } from 'expo-checkbox';
-import React from 'react';
-import { FlatList, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Checkbox } from "expo-checkbox";
+import { useEffect, useState } from "react";
+import { FlatList, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
-  const todoData = [
-    { id: 1, title: "8 bardak su iç", completed: true },
-    { id: 2, title: "yürüyüşe çık", completed: true },
-    { id: 3, title: "ders çalış", completed: false },
-    { id: 4, title: "nefes egzersizi yap", completed: false },
-    { id: 5, title: "şekersiz beslen", completed: false },
+type ToDoType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+export default function Index() { //Uygulama açıldığında gösterilecek ekran.
+  const todoData = [ //Sadece test amaçlı, örnek todo listesi.
+    {id: 1, title: "Todo 1", isDone: false},
+    {id: 2, title: "Todo 2", isDone: true},
+    {id: 3, title: "Todo 3", isDone: false},  
+    {id: 4, title: "Todo 4", isDone: true},
+    {id: 5, title: "Todo 5", isDone: false},
   ];
 
+  //State Tanımları:
+  //useState: Bileşenin içinde değişebilen (dinamik) verileri tutar.
+  const [todos, setTodos] = useState<ToDoType[]>([]); //Uygulamadaki aktif görevleri tutar.
+  const [todoText, setTodoText] = useState<string>(""); //Kullanıcının girdiği yeni todo metnini tutar.
+  const [searchQuery, setSearchQuery] = useState<string>(""); //Arama çubuğundaki metni saklar.
+  const [oldTodos, setOldTodos] = useState<ToDoType[]>([]); //Filtreleme (arama) yaparken orijinal veriyi korumak için tutulur.
+
+  //useEffect: Uygulama açıldığında veya belirli durumlar değiştiğinde bir fonksiyon çalıştırır (örn. veriyi yüklemek). Amaç: Uygulama açıldığında AsyncStorage’dan kayıtlı veriyi çekmek.
+  useEffect(() => { //Sadece ilk render'da çalışır.
+    const getTodos = async () => {
+      try {
+        const todos = await AsyncStorage.getItem("my-todo");
+        if (todos !== null) {
+          setTodos(JSON.parse(todos));
+          setOldTodos(JSON.parse(todos));
+          //JSON.parse: Saklanan metni (string) diziye çevirir.
+          // setTodos ve setOldTodos: Listeyi ekrana yansıtır.
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTodos();
+  }, []);
+
+  const addTodo = async () => {
+    try {
+      const newTodo = {
+        id: Math.random(), //Benzersiz ID oluşturur.
+        title: todoText,
+        isDone: false,
+      };
+      const newTodos = [...todos, newTodo];
+      setTodos(newTodos);
+      setOldTodos(todos);
+      await AsyncStorage.setItem("my-todo", JSON.stringify(todos)); //Güncel listeyi kaydeder.
+      setTodoText("");
+      Keyboard.dismiss(); //Klavyeyi kapatır.
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //ID’si eşleşmeyen todo’ları filtreleyip yenisini kaydeder:
+  const deleteTodo = async (id: number) => {
+    try {
+      const newTodos = todos.filter((todo) => todo.id !== id);
+      await AsyncStorage.setItem("my-todo", JSON.stringify(newTodos));
+      setTodos(newTodos);
+      setOldTodos(newTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Todo tamamlandı olarak işaretlemek:
+  const handleDone = async (id: number) => {
+    try {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.isDone = !todo.isDone;
+        }
+        return todo;
+      });
+      await AsyncStorage.setItem("my-todo", JSON.stringify(newTodos));
+      setTodos(newTodos);
+      setOldTodos(newTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Arama fonksiyonu:
+  const onSearch = (query: string) => {
+    if (query == "") {
+      setTodos(oldTodos);
+    } else {
+      const filteredTodos = todos.filter((todo) =>
+        todo.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setTodos(filteredTodos);
+    }
+  };
+
+  //Kullanıcı arama kutusuna her yazdığında filtreleme çalışır:
+  useEffect(() => 
+    {onSearch(searchQuery);},
+    [searchQuery]);
+
   return (
+    //SafeAreaView: Ekranın güvenli alanını kullanarak içeriğin kesilmesini önler.
+    <SafeAreaView style={styles.container}>
+      {/* Header: Üst kısım, menü butonu vs. */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name="menu" size={24} color={"#333"} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}></TouchableOpacity>
+      </View>
 
-  <SafeAreaView style={styles.container}>
-    <View style={styles.header}>
-      <TouchableOpacity onPress={() => {}}>
-        <Ionicons name="menu" size={24} color="black" /> 
-      </TouchableOpacity>
-    </View>
-    <View style={styles.searchBar}>
-      <Ionicons name="search" size={24} color="black" />
-      <TextInput placeholder='Search'/>
-    </View>
-    
-    <FlatList
-      data={todoData}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View style={styles.todoContainer}>
-          <View style={styles.todoInfoContainer}>
-          <Checkbox value={item.completed} color={item.completed ? "#7d4df6ff" : undefined}></Checkbox>
-          <Text style={[styles.todoText, item.completed && {textDecorationLine: "line-through"}]}>{item.title}</Text>
-          </View>
-          <TouchableOpacity onPress={() => {}}>
-          <Ionicons name="trash" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-      )}
-    />
+      {/* Arama Çubuğu */}
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={24} color={"#333"} />
+        <TextInput
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={(text) => setSearchQuery(text)}
+          style={styles.searchInput}
+          clearButtonMode="always"
+        />
+      </View>
 
-
-    <KeyboardAvoidingView style={styles.footer}>
-      <TextInput placeholder='New To Do' style={styles.newTodoInput}/>
-      <TouchableOpacity style={styles.addButton} onPress={() => {}}>
-        <Ionicons name="add" size={24} color="white" />
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
-  </SafeAreaView>
-);
+      {/* FlatList: Uzun liste verilerini performanslı bir şekilde göstermek için kullanılır. */}
+      <FlatList
+        data={[...todos].reverse()} //Son eklenen en üstte görünür.
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ToDoItem
+            todo={item}
+            deleteTodo={deleteTodo}
+            handleDone={handleDone}
+          />
+        )}
+      />
+      
+      {/*KeyboardAvoidingView: Klavye açıldığında bileşenleri yukarı kaydırır (Input gizlenmesin diye).*/}
+      <KeyboardAvoidingView 
+        style={styles.footer}
+        behavior="padding"
+        keyboardVerticalOffset={10}
+      >
+        <TextInput
+          placeholder="Add New ToDo"
+          value={todoText}
+          onChangeText={(text) => setTodoText(text)}
+          style={styles.newTodoInput}
+          autoCorrect={false}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={() => addTodo()}>
+          <Ionicons name="add" size={34} color={"#fff"} />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
 
+//ToDoItem: Her bir todo öğesini temsil eden alt bileşen.
+const ToDoItem = ({
+  todo,
+  deleteTodo,
+  handleDone,
+}: {
+  todo: ToDoType;
+  deleteTodo: (id: number) => void;
+  handleDone: (id: number) => void;
+}) => (
+  <View style={styles.todoContainer}>
+    <View style={styles.todoInfoContainer}>
+      <Checkbox
+        value={todo.isDone}
+        onValueChange={() => handleDone(todo.id)}
+        color={todo.isDone ? "#4630EB" : undefined}
+      />
+      <Text
+        style={[
+          styles.todoText,
+          todo.isDone && { textDecorationLine: "line-through" },
+        ]}
+      >
+        {todo.title}
+      </Text>
+    </View>
+    <TouchableOpacity
+      onPress={() => {
+        deleteTodo(todo.id);
+        alert("Deleted " + todo.id);
+      }}
+    >
+      <Ionicons name="trash" size={24} color={"red"} />
+    </TouchableOpacity>
+  </View>
+);
+
+{/*StyleSheet: Stil tanımlamalarını yapar ve performans optimizasyonu sağlar*/}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
+    backgroundColor: "#f5f5f5",
   },
-  header:{
-    flexDirection:"row",
-    alignItems:"center",
-    marginBottom:20,
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
   },
-  searchBar:{
-    flexDirection:"row",
-    backgroundColor:"#ffffffe0",
-    padding: 5,
-    borderRadius:15,
-    gap:10,
-    marginBottom:20,
+  searchBar: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: Platform.OS === 'ios' ? 16 : 8,
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 20,
   },
-  todoContainer:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    backgroundColor:"#ffffffe0",
-    padding:10,
-    borderRadius: 15,
-    gap:10,
-    marginBottom:10,
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
   },
-  todoInfoContainer:{
-    flexDirection:"row",
-    gap:10,
-    alignItems:"center", //ne için bak
+  todoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 20,
   },
-  todoText:{
-    fontSize:16,
-    color:"#000",
+  todoInfoContainer: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
   },
-  footer:{
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-between",
+  todoText: {
+    fontSize: 16,
+    color: "#333",
   },
-  newTodoInput:{
-    flex:1,
-    backgroundColor:"#ffffffe0",
-    padding:10,
-    borderRadius:15,
-    fontSize:16,
-    color:"#000",
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    bottom: 20,
   },
-  addButton:{
-    backgroundColor:"#7d4df6ff",
-    padding:10,
-    borderRadius:15,
-    marginLeft:10,
-  }
-
+  newTodoInput: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    fontSize: 16,
+    color: "#333",
+  },
+  addButton: {
+    backgroundColor: "#4630EB",
+    padding: 8,
+    borderRadius: 10,
+    marginLeft: 20,
+  },
 });
